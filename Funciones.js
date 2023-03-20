08a310ab74ad0ea3c4dd2ba07c082008c52d6534
sebastianroja <sebastian.android.norambuena@gmail.com>
2023-03-20 20:18:54 -0300

    carpeta de funcion y documento nacimiento

--- Funciones.js
+++ Funciones.js
@@ -1,13 +1,35 @@
-var password = Login.getElementById("password")
-  , confirm_password = Login.getElementById("confirm_password");
 
-function validatePassword (){
-  if(password.value != confirm_password.value) {
-    Login.confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    Login.confirm_password.setCustomValidity('');
-  }
-}
+    
+    console.log('hola')
+    function validatePassword (){
+      var password = document.getElementById("pass"), 
+    confirm_password = document.getElementById("confirm_password")
+      if(password.value != confirm_password.value) {
+        console.log("novale")
+        document.getElementById("confirm_password").value=""
+        alert("Las contraseñas debn ser iguales");
+      } else {
+        
+        console.log("iguales")
+      }
+    }
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+   
+      function calcularEdad(fecha_nacimiento) {
+        var hoy = new Date();
+        var cumpleanos = new Date(fecha_nacimiento);
+        var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+        var m = hoy.getMonth() - cumpleanos.getMonth();
+        if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+            edad--;
+        }
+        return edad;
+    }
+    
+    var edad = calcularEdad();
+    if(edad >= 18){
+        alert("Eres mayor de edad :D ");
+    }else{
+        alert("Eres menor de edad :( ");
+    }
+    
